40f16f72c4963bf9055b788fe6ebcd299d042dde
classic12 <MacHome@home.home>
2014-09-19 10:55:31 +0100

    RFID Test app
    Test app

---
+++ Resources/D1aconfig.js
@@ -0,0 +1,16 @@
+AC.Config.set({
+    apiAdapter : "Web",
+    needFlashVersion : "9.0.0",
+    clientApi : "https://classic.applicationcraft.com/",
+    serverUrl : "https://classic.applicationcraft.com/service/",
+    serverUploadUrl : "https://classic.applicationcraft.com/service/",
+    uploadServerPath: "https://classic.applicationcraft.com/service/uploads/",
+    uploadServerDirectPath: "https://classic.applicationcraft.com/service/uploads/direct.php",
+    serverEventsUrl: 'https://notify.applicationcraft.com/events',
+    formatApi : "json",
+    serverThemes: "themes/",
+    localAppsPath: 'Apps/',
+    flashAdapterUrl : "wiziCore/api/server/adapter/WiziSendRequest.swf",
+    isDebugRequest : true,
+    googleApiKey: {"rows":[{"id":1,"data":["http://applicationcraft.com/","ABQIAAAACfMroKb5WzfdVTHbQi2OfRRGQFuqEDC-bwWLPZ60Ces-G56XXhTiOZYFBXlh0_EId2nXJCwq7iTNIg"],"ind":1}]}
+});
